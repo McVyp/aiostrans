c46459803c2775e358869025f9513f46271bf2fa
McVyp <93302780+McVyp@users.noreply.github.com>
2023-04-15 21:03:33 +0545

    add open api keu

---
+++ scripts/transcribe.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
+video_id = sys.argv[1]
+audio_file = os.path.join(os.getcwd(), "tmp", video_id + ".m4a")
+
+print(audio_file)
