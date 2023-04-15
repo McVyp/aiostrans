f9157f4373c33971d6f01a801e2aae8a55f2f33f
McVyp <93302780+McVyp@users.noreply.github.com>
2023-04-16 00:58:42 +0545

    remove env

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -3,6 +3,13 @@ import sys
 import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 video_id = sys.argv[1]
-audio_file = os.path.join(os.getcwd(), "tmp", video_id + ".m4a")
+audio_file_path = os.path.join(os.getcwd(), "tmp", video_id + ".m4a")
 
-print(audio_file)
+audio_file = open(audio_file_path, 'rb')
+transcript = openai.Audio.transcribe(
+    file=audio_file,
+    model= "whisper-1",
+    response_format="srt",
+    prompt = 'I am a devekoper'
+)
+print(transcript)
