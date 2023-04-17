5267f5c988fbb9944e4641e45dae412e12cb04bf
McVyp <93302780+McVyp@users.noreply.github.com>
2023-04-17 21:29:10 +0545

    add test transcribepy

---
+++ scripts/transcribe.py
@@ -0,0 +1,8 @@
+import os
+import openai
+import sys
+openai.api_key = os.getenv('OPENAI_API_KEY')
+video_id = sys.argv[1]
+audio_file = os.path.join(os.getcwd(), 'ast', video_id + '.m4a')
+
+print(audio_file)
